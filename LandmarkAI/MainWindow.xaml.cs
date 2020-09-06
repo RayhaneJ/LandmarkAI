04bdfc7a3613308f98dbda33836668c00121137d
rayhaneJ <jebbarirayhane@gmail.com>
2020-09-06 15:32:24 +0200

    LandmarkAI

---
+++ LandmarkAI/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using LandmarkAI.Models;
+using Microsoft.Win32;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LandmarkAI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Image files (*.png; *.jpg)|*.png;*.jpg;*jpeg|All files (*.*)|*.*";
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+
+            if (dialog.ShowDialog() == true)
+            {
+                string fileName = dialog.FileName;
+                selectedImage.Source = new BitmapImage(new Uri(fileName));
+
+                MakePredictionAsync(fileName);
+            }
+
+        }
+
+        private async void MakePredictionAsync(string fileName)
+        {
+            string url = "url";
+            string predictionKey = "";
+            string contentType = "application/octet/stream";
+            var file = File.ReadAllBytes(fileName);
+
+            using (HttpClient httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Add("Prediciton-Key", predictionKey);
+
+                using (var content = new ByteArrayContent(file))
+                {
+                    content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(contentType);
+
+                    var response = await httpClient.PostAsync(url, content);
+
+                    var responseString = await response.Content.ReadAsStringAsync();
+
+                    List<Prediction> predictions = JsonConvert.DeserializeObject<CustomVision>(responseString).Predictions.ToList();
+                    predictionsListView.ItemsSource = predictions;
+                }
+            }
+        }
+    }
+}
---
+++ LandmarkAI/Models/CustomVision.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LandmarkAI.Models
+{
+    public class Prediction
+    {
+        public string TagId { get; set; }
+        public string Tag { get; set; }
+        public string Probability { get; set; }
+    }
+
+    public class CustomVision
+    {
+        public string Id { get; set; }
+        public string Project { get; set; }
+        public string Iteration { get; set; }
+        public DateTime Created { get; set; }
+        public IList<Prediction> Predictions { get; set; }
+    }
+}
